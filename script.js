aa1d786bba847c214f282d4c5e4fac0cd5c7d6db
Dmitriy Koval <dimakovalvdv@gmail.com>
2020-07-12 22:15:42 +0300

    feat: modify js file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -7,13 +9,29 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+let i = 0;
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-const raitingFilm = prompt('На сколько оцените его?', '');
-const lastFilm1 = prompt('Один из последних просмотренных фильмов?', '');
-const raitingFilm2 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+    const raitingFilm = prompt('На сколько оцените его?', '');
+    
+    if (lastFilm != null && raitingFilm != null && lastFilm != '' && raitingFilm != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = raitingFilm;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[lastFilm] = raitingFilm;
-personalMovieDB.movies[lastFilm1] = raitingFilm2;
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else (
+    alert('Произошла ошибка')
+)
 
 console.log(personalMovieDB);
